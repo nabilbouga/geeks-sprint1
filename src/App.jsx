78cdf452ed645aae231823d9d99a8f06d8cc83ab
Oussama Naouaoui <naouaouioussama@gmail.com>
2025-05-28 23:32:48 +0100

    TopDestinations & img & destinations

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HowItWorks from './components/sections/HowItWorks';
+import TopDestinations from './components/sections/TopDestinations';
 
 function App() {
   return (
     <div>
         <HowItWorks />
+        <TopDestinations/>
     </div>
   );
 }
