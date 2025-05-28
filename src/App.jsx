59c6c93bb6c85c547fcf54c7e910a459e0624a62
Ayozou <ayoubzoubiriyc@gmail.com>
2025-05-28 23:42:21 +0100

    Footer az

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import HowItWorks from './components/sections/HowItWorks';
+import Footer from './components/sections/Footer';
 
 function App() {
   return (
     <div>
         <HowItWorks />
+        <Footer />
+
+        
     </div>
   );
 }
